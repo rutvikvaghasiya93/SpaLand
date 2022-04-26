5b91317eda26cb0b4de7c54b9b3587652c93d95d
rutvikvaghasiya93 <rutvikvaghasiya93@gmail.com>
2022-04-26 11:21:59 -1200

    change in design

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+$(document).scroll(function(){
+  $('.navbar-collapse').collapse('hide');
+});
+});
+
+$(document).on('click',function(){
+  $('.collapse').collapse('hide');
+});
---
+++ centerlogo/assets/js/custom.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+$(document).scroll(function(){
+  $('.navbar-collapse').collapse('hide');
+});
+});
+$(document).on('click',function(){
+  $('.collapse').collapse('hide');
+});
